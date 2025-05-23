3db1062bfa5379c25862cc7393fb99d15362acfd
mhi1627 <pvg1627@gmail.com>
2025-05-23 20:29:05 +0530

    landing page ready

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,32 +1,36 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { contentImg } from '../public'
-
+import { IoIosArrowRoundForward } from "react-icons/io";
 export default function Hero() {
   return (
-    <div className='relative  h-screen w-full mt-8 overflow-hidden '>
-      <div id='headings' className='px-3 bg-red-500'>
+    <div className='relative h-screen w-full mt-8 overflow-hidden '>
+      <div id='headings' className='px-3 xl:p-16 xl:mt-7  '>
         {["We create", "Eye-opening", "presentations"].map((heading,index)=>(
           <div className=''>
             {index === 1 ?
             <span className='relative before:absolute flex gap-2 items-end '>
-             <div className='rounded-lg overflow-hidden'>
-              <img src={contentImg} alt="" className=' object-cover lg:h-[5.6rem] md:h-[5rem] sm:h-[3.6rem] h-[2.5rem]' />
+             <div className='rounded-lg overflow-hidden w-1/10'>
+              <img src={contentImg} alt="" className=' object-cover lg:h-[5.6rem] md:h-[5rem] sm:h-[3.6rem] h-[2.5rem] xl:h-[7.3rem]' />
               </div> 
-            <h1 className='text-nowrap text-[4rem] leading-12 sm:text-[6rem] sm:leading-16 md:text-[8rem] md:leading-23  lg:text-[9rem] lg:leading-26  uppercase foundersGrotesk  '>{heading}</h1>
+            <h1 className=' text-nowrap text-[4rem] leading-12 sm:text-[6rem] sm:leading-16 md:text-[8rem] md:leading-23  lg:text-[9rem] lg:leading-26 xl:text-[11rem] xl:leading-33  uppercase foundersGrotesk  '>{heading}</h1>
             </span> :
-            <h1 className='text-[4rem] leading-12 sm:text-[6rem] sm:leading-16 md:text-[8rem] md:leading-23  lg:text-[9rem] lg:leading-26  uppercase foundersGrotesk  '>{heading}</h1>
+            <h1 className='text-[4rem] leading-12 sm:text-[6rem] sm:leading-16 md:text-[8rem] md:leading-23  lg:text-[9rem] lg:leading-26 xl:text-[11rem]  xl:leading-33 uppercase foundersGrotesk  '>{heading}</h1>
             }
           </div>
         ))}
       </div>
-      <div className=' mt-[9.5rem] border-t  border-gray-400'>
-         <h1>
-          Lorem, ipsum dolor.
-         </h1>
-         <h1>
-          Lorem, ipsum dolor.
-         </h1>
+      <div className='lg:flex justify-between mt-[9.5rem] xl:mt-[5rem] border-t  border-gray-400 px-[4rem] py-[1.4rem]'>
+          <h3 className='text-[1.3rem]'>For public and private companies</h3>
+          <h3 className='text-[1.3rem]'>From the first pitch to IPO</h3>
+         <span className='flex items-center gap-2'>
+         <button className='uppercase text-[1.2rem] rounded-4xl border p-1 px-3 border-black '>
+          Start the Project
+         </button>
+           <span className='border flex items-center  border-black rounded-full '>
+           <IoIosArrowRoundForward className='w-9 h-9 -rotate-45'/>
+           </span>
+         </span>
       </div>
     </div>
   )
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ import { NavItems } from './constants'
 
 export default function Navbar() {
   return (
-    <div className='h-12 w-full flex justify-between items-center px-[1.1rem] py-[2rem]'>
+    <div className='h-12 w-full flex justify-between items-center px-[1.1rem] py-[2rem] xl:px-[4rem] xl:py-[2.2rem] '>
       <svg width="72" height="30" viewBox="0 0 72 30" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<path d="M9.8393 10.2032C4.22951 10.3257 -0.0459221 14.7356 0.000372391 20.2752C0.0412204 25.3548 4.57808 30.3608 10.6862 29.9226C15.5145 29.5768 19.9015 25.4119 19.8525 20.0057C19.8035 14.5995 15.1904 10.0916 9.8393 10.2032ZM9.89649 25.7005C6.87101 25.7005 4.39834 23.1144 4.40924 19.9839C4.39525 19.2507 4.52792 18.522 4.79947 17.8407C5.07102 17.1594 5.47597 16.5392 5.99056 16.0164C6.50515 15.4937 7.11902 15.0789 7.79613 14.7966C8.47324 14.5142 9.19995 14.3698 9.93362 14.372C10.6673 14.3742 11.3931 14.5228 12.0686 14.8092C12.744 15.0956 13.3554 15.514 13.8668 16.0398C14.3783 16.5656 14.7796 17.1882 15.0471 17.8711C15.3146 18.554 15.4429 19.2834 15.4246 20.0166C15.4409 23.1008 12.9111 25.7059 9.88832 25.7005H9.89649Z" fill="currentColor"></path>
 									<path d="M62.8086 29.4855H67.1222V10.6372H62.8086V29.4855Z" fill="currentColor"></path>
@@ -15,10 +15,10 @@ export default function Navbar() {
 								</svg>
      <ul className='lg:flex gap-5 lg:ml-[23rem] hidden '>
         {NavItems.map(item => (
-        <li className='text-[1.1rem] NeueMontreal'>{item}</li>
+        <li className='text-[1.1rem] xl:text-[1.3rem] NeueMontreal'>{item}</li>
      ))}
      </ul>
-     <button className='text-[1.1rem] NeueMontreal lg:block hidden'>Contact Us</button>
+     <button className='text-[1.1rem] NeueMontreal lg:block hidden xl:text-[1.3rem]'>Contact Us</button>
 
      <svg class="nav-toggle" width="24" height="9" viewBox="0 0 24 9" fill="none" xmlns="http://www.w3.org/2000/svg" className='lg:hidden'>
 									<line x1="24" y1="8" x2="-4.37114e-08" y2="8" stroke="currentColor"></line>
